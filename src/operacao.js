fb6ab20c4a80f9fa23de188c0ced593510f46b3a
Raiane Miranda <raianemirandela@gmail.com>
2023-02-21 15:45:36 -0300

    criação da operação de divisão e raiz

--- src/operacao.js
+++ src/operacao.js
@@ -1,26 +1,38 @@
 function sum(num1, num2){
     if(isNaN(num1)||isNaN(num2)){
         throw 'Não é um número'
-    }
-    return num1+num2
+    } return num1+num2
 }
 
 function sub(num1, num2){
     if(isNaN(num1)||isNaN(num2)){
         throw 'Não é um número'
-    }
-    return num1-num2
+    } return num1-num2
 }
 
 function mult(num1, num2){
     if(isNaN(num1)||isNaN(num2)){
         throw 'Não é um número'
-    }
-    return num1*num2
+    } return num1*num2
 }
+
+function div(num1,num2){
+    if (isNaN(num1)||isNaN(num2)){
+        throw 'Não é um número'
+    } return num1/num2
+}
+
+function raiz(num1){
+    if(isNaN(num1)){
+        throw "Não é um número"
+    }  return Math.sqrt(num1)
+}
+
 
 module.exports ={
     sum,
     sub,
-    mult
+    mult,
+    div,
+    raiz
 }
--- test/operacao.test.js
+++ test/operacao.test.js
@@ -38,22 +38,70 @@ describe('Teste da calculadora', () => {
     })
 
     it('Operação de Multiplicação', () => {
-        
-        let resultado = calculadora.mult(1,2)
+
+        let resultado = calculadora.mult(1, 2)
         expect(resultado).toEqual(2)
 
         resultado = calculadora.mult(-1, 2)
         expect(resultado).toEqual(-2)
 
+        resultado = calculadora.mult(0.22, 2)
+        expect(resultado).toEqual(0.44)
+       
         resultado = calculadora.mult(0, 2)
         expect(resultado).toEqual(0)
+       
+        resultado = calculadora.mult(6/2, 2)
+        expect(resultado).toEqual(6)
+        
+        resultado = calculadora.mult(2, 6/2)
+        expect(resultado).toEqual(6)
 
         resultado = () => calculadora.mult("a", 2);
         expect(resultado).toThrow('Não é um número')
 
         resultado = () => calculadora.mult(2, "a");
         expect(resultado).toThrow('Não é um número')
+
+
+    })
+
+    it('Operação de Divisão', () => {
+
+        let resultado = calculadora.div(2, 1)
+        expect(resultado).toEqual(2)
+
+        resultado = calculadora.div(1.22, 2)
+        expect(resultado).toEqual(0.61)
        
+        resultado = calculadora.div(2, 1.22)
+        expect(resultado).toEqual(1.639344262295082)
+        
+        resultado = calculadora.div(-6,2)
+        expect(resultado).toEqual(-3)
+
+        resultado = calculadora.div(0, 2)
+        expect(resultado).toEqual(0)
+        
+        resultado = calculadora.div(2/4, 2)
+        expect(resultado).toEqual(0.25)
+       
+        resultado = calculadora.div(2, 2/8)
+        expect(resultado).toEqual(8)
+
+        resultado = () => calculadora.div("a", 2);
+        expect(resultado).toThrow('Não é um número')
+
+        resultado = () => calculadora.div(2, "a");
+        expect(resultado).toThrow('Não é um número')
+
+
+    })
+
+    it('Operação de Raiz', () => {
+
+        let resultado = calculadora.raiz(16)
+        expect(resultado).toEqual(4)
 
     })
 
